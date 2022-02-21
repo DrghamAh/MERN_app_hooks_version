95f1c0a59c52a8e7dc1608b69098102ed0a56cfb
Drgham_Abou_Heassoun <drghamah2@email.com>
2022-02-21 05:49:29 -0600

    Edit upload image component

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,23 +1,49 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function UploadImage() {
-  // const [image, setImage] = useState()
+  const [image, setImage] = useState('');
+  const [imageName, setImageName] = useState('Choose File');
 
-  const handleFormSubmit = (e) => {
-    console.log(e);
+  const handleFileChange = (e) => {
+    setImage(e.target.files[0]);
+    setImageName(e.target.files[0].name);
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('image', image);
+    formData.append('imageName', imageName);
+    try {
+      const response = await axios.post('http://localhost:5000/images', formData, {
+        headers : {
+          'Content-type' : 'multipart/form-data',
+        }
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <div>
-      <form encType='multipart/form-data' onSubmit={handleFormSubmit}>
-        <div className="form-group">
-          <label htmlFor="image">Image</label>
-          <input type="file" name="image" className="form-control"/>
-        </div>
-        <input type='text' name='name' className='form-control' />
-        <button type="submit" className='btn btn-primary'>Submit</button>
-      </form>
-    </div>
+    <Fragment>
+      <div className="container">
+        <h1 className="text-center my-4">
+          <i className="fab fa-react"></i>
+          React Upload File
+        </h1>
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-group custom-file">
+            <input type="file" name="image" className="custom-file-input" id="customFile" onChange={handleFileChange}/>
+            <label htmlFor='customFile' className='custom-file-label d-block'>{imageName}</label>
+          </div>
+          <button type="submit" className='btn btn-primary d-block my-4'>Submit</button>
+        </form>
+      </div>
+    </Fragment>
   )
 }
 
